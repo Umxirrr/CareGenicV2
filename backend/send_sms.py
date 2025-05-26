a0326ff726e3ca45bed090763e592007e13366e9
umair <shaikhumair9067@gmail.com>
2025-05-26 21:34:55 +0530

    send_sms.py

--- backend/send_sms.py
+++ backend/send_sms.py
@@ -2,9 +2,14 @@ import boto3
 
 def send_sms(phone_number, message):
     try:
-        sns = boto3.client("sns", region_name="ap-south-1")  # Use Mumbai region
+        sns = boto3.client(
+            "sns",
+            region_name="ap-south-1",
+            aws_access_key_id="AKIAUUHAPO3WDAWOLU6H",
+            aws_secret_access_key="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"
+        )
         response = sns.publish(
-            PhoneNumber=phone_number,  # Must be in +91XXXXXXXXXX format
+            PhoneNumber=phone_number,
             Message=message
         )
         return True, response['MessageId']
